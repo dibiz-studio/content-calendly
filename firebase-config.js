6507292f2cbd74e0e5808be8dc46f02ac2512505
dibiz-studio <tech@dibizsolution.com>
2026-06-30 15:34:42 +0530

    photo add

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAmpAKp6lsXHE1UaJseGEBRzNUE5Ek_IQ",
+  authDomain: "content-calendly.firebaseapp.com",
+  projectId: "content-calendly",
+  storageBucket: "content-calendly.firebasestorage.app",
+  messagingSenderId: "469206946182",
+  appId: "1:469206946182:web:f982a395cdfbabe0a2dc07",
+  measurementId: "G-TK48S254EQ"
+};
